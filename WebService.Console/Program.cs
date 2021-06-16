b4a897813623872cffc7a476d051b3c6b84a2dec
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2021-06-17 01:01:43 +0200

    Add project files.

---
+++ WebService.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using WebService.Console.CountryWebService;
+
+namespace WebService.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountryInfoServiceSoapTypeClient client = 
+                new CountryInfoServiceSoapTypeClient("CountryInfoServiceSoap");
+
+            var response = client.ListOfCountryNamesByName();
+            foreach (var data in response)
+            {
+               System.Console.WriteLine(data.sName);   
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
